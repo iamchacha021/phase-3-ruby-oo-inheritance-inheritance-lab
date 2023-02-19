18325a66fb4fa4eb8aa25782de4e202373933442
iamchacha021 <iamchacha021@gmail.com>
2023-02-19 18:08:32 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,26 @@
-class Student 
+require 'pry'
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(str)
+        @knowledge << str
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
+
+st1 = Student.new
+st1.first_name = 'bx'
+st1.last_name = 'lt'
+
+st1.learn('maths')
+st1.learn('eng')
+st1.learn('swa')
+
+
+binding.pry
